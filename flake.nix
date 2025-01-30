101ec71cf528dbd070c61d904efdcac547f0d1cb
InvraNet <identificationsucks@gmail.com>
2025-01-30 19:31:43 +1100

    Add nix.

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+    description = "Basic flake for Astro.js and Bun.js project";
+
+    inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+        let
+        pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+        devShells.default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+                bun
+                nodejs
+            ];
+
+            shellHook = ''
+                echo 'Run bun run to get the commands to get started.'
+            '';
+        };
+    });
+}
